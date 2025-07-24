1e8389a7f5bc07f65de927b30fcd7ced58998795
Natsumi Teshima <nateshim@c1r2s2.42tokyo.jp>
2025-07-24 19:47:59 +0900

    modified: CPP00

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -2,77 +2,78 @@
 
 bool Contact::setContact()
 {
-  std::cout << "First name     : ";
-  std::getline(std::cin, this->_firstName);
-  if (this->_firstName.empty()) return false;
-
-  std::cout << "Last name      : ";
-  std::getline(std::cin, this->_lastName);
-  if (this->_lastName.empty()) return false;
-
-  std::cout << "Nickname       : ";
-  std::getline(std::cin, this->_nickname);
-  if (this->_nickname.empty()) return false;
-
-  std::cout << "Phone number   : ";
-  std::getline(std::cin, this->_phoneNumber);
-  if (this->_phoneNumber.empty()) return false;
-
-  std::cout << "Darkest secret : ";
-  std::getline(std::cin, this->_secret);
-  if (this->_secret.empty()) return false;
-
-  return true;
+	std::cout << "First name     : ";
+	std::getline(std::cin, this->_firstName);
+	if (this->_firstName.empty())
+		return (false);
+	std::cout << "Last name      : ";
+	std::getline(std::cin, this->_lastName);
+	if (this->_lastName.empty())
+		return (false);
+	std::cout << "Nickname       : ";
+	std::getline(std::cin, this->_nickname);
+	if (this->_nickname.empty())
+		return (false);
+	std::cout << "Phone number   : ";
+	std::getline(std::cin, this->_phoneNumber);
+	if (this->_phoneNumber.empty())
+		return (false);
+	std::cout << "Darkest secret : ";
+	std::getline(std::cin, this->_secret);
+	if (this->_secret.empty())
+		return (false);
+	return (true);
 }
 
-
-Contact::Contact(void){
-    std::cout << "\033[90mConstructor for Contact called\033[0m" << std::endl;
-}
-
-Contact::~Contact(void){
-    std::cout << "\033[90mDestructor for Contact called\033[0m" << std::endl;
-}
-
-void Contact::savecontact(std::string firstName, std::string lastName, std::string nickname, std::string phoneNumber, std::string secret, int index)
+Contact::Contact(void)
 {
-    this->_firstName = firstName;
-    this->_lastName = lastName;
-    this->_nickname = nickname;
-    this->_phoneNumber = phoneNumber;
-    this->_secret = secret;
-    this->_index = index;
+	std::cout << "\033[90mConstructor for Contact called\033[0m" << std::endl;
+}
+
+Contact::~Contact(void)
+{
+	std::cout << "\033[90mDestructor for Contact called\033[0m" << std::endl;
+}
+
+void Contact::savecontact(std::string firstName, std::string lastName,
+	std::string nickname, std::string phoneNumber, std::string secret,
+	int index)
+{
+	this->_firstName = firstName;
+	this->_lastName = lastName;
+	this->_nickname = nickname;
+	this->_phoneNumber = phoneNumber;
+	this->_secret = secret;
+	this->_index = index;
 }
 
 std::string Contact::tenchar(std::string test) const
 {
-    std::string substring;
-    std::string to_write;
+	std::string substring;
+	std::string to_write;
 
-    if (test.length() > 10) {
-        substring = test.substr(0, 9);
-        to_write = substring + ".";
-    }
-    else
-        to_write = test;
-    return (to_write);
+	if (test.length() > 10)
+	{
+		substring = test.substr(0, 9);
+		to_write = substring + ".";
+	}
+	else
+		to_write = test;
+	return (to_write);
 }
 
 void Contact::showlittle(void) const
 {
-    std::cout << "| " << std::setw(10) << std::right << this->_index << "| "
-          << std::setw(10) << std::right << tenchar(this->_firstName) << "| "
-          << std::setw(10) << std::right << tenchar(this->_lastName) << "| "
-          << std::setw(10) << std::right << tenchar(this->_nickname) << "|\n";
+	std::cout << "| " << std::setw(10) << std::right << this->_index << "| " << std::setw(10) << std::right << tenchar(this->_firstName) << "| " << std::setw(10) << std::right << tenchar(this->_lastName) << "| " << std::setw(10) << std::right << tenchar(this->_nickname) << "|\n";
 }
 
-void Contact::showall(void) const 
+void Contact::showall(void) const
 {
-    std::cout << "----------------------------" << std::endl;
-    std::cout << "- First name          :" << this->_firstName << std::endl;
-    std::cout << "- Last name           :" << this->_lastName << std::endl;
-    std::cout << "- Nickname            :" << this->_nickname << std::endl;
-    std::cout << "- Phone Number:       :" << this->_phoneNumber << std::endl;
-    std::cout << "- Darkest secret      :" << this->_secret << std::endl;
-    std::cout << "----------------------------" << std::endl;
+	std::cout << "----------------------------" << std::endl;
+	std::cout << "- First name          :" << this->_firstName << std::endl;
+	std::cout << "- Last name           :" << this->_lastName << std::endl;
+	std::cout << "- Nickname            :" << this->_nickname << std::endl;
+	std::cout << "- Phone Number:       :" << this->_phoneNumber << std::endl;
+	std::cout << "- Darkest secret      :" << this->_secret << std::endl;
+	std::cout << "----------------------------" << std::endl;
 }
--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -1,72 +1,81 @@
 #include "PhoneBook.hpp"
 #include <stdio.h>
 
-PhoneBook::PhoneBook(int value) : _size(value), _full(value) {
-  std::cout << "\033[90mConstructor for PhoneBook called\033[0m" << std::endl;
-}
-
-PhoneBook::~PhoneBook(void) {
-  std::cout << "\033[90mDestructor for PhoneBook called\033[0m" << std::endl;
-}
-
-int PhoneBook::getsize(void) {
-  return this->_size;
-}
-
-void PhoneBook::setsize(void) {
-  if (this->getsize() == 8)
-    this->_size = 0;
-}
-
-int PhoneBook::getfull(void) {
-  return this->_full;
-}
-
-void PhoneBook::setfull(void) {
-  if (this->getfull() == 8)
-    this->_full = 7;
-}
-
-void PhoneBook::add_repertory(std::string firstName, std::string lastName, std::string nickname, std::string phoneNumber, std::string secret)
+PhoneBook::PhoneBook(int value) : _size(value), _full(value)
 {
-  this->setsize();
-  this->setfull();
-  this->_contacts[_size].savecontact(firstName, lastName, nickname, phoneNumber, secret, getsize());
-  _size++;
-  _full++;
+	std::cout << "\033[90mConstructor for PhoneBook called\033[0m" << std::endl;
 }
 
-bool PhoneBook::add_repertory() {
-  std::string firstName, lastName, nickname, phoneNumber, secret;
+PhoneBook::~PhoneBook(void)
+{
+	std::cout << "\033[90mDestructor for PhoneBook called\033[0m" << std::endl;
+}
 
-  std::cout << "First name     : ";
-  std::getline(std::cin, firstName);
-  std::cout << "Last name      : ";
-  std::getline(std::cin, lastName);
-  std::cout << "Nickname       : ";
-  std::getline(std::cin, nickname);
-  std::cout << "Phone number   : ";
-  std::getline(std::cin, phoneNumber);
-  std::cout << "Darkest secret : ";
-  std::getline(std::cin, secret);
+int PhoneBook::getsize(void)
+{
+	return (this->_size);
+}
 
-  if (firstName.empty() || lastName.empty() || nickname.empty() || phoneNumber.empty() || secret.empty()) {
-    std::cout << "\033[31mPlease do not leave fields empty.\033[0m" << std::endl;
-    return false;
-  }
+void PhoneBook::setsize(void)
+{
+	if (this->getsize() == 8)
+		this->_size = 0;
+}
 
-  this->add_repertory(firstName, lastName, nickname, phoneNumber, secret);
-  return true;
+int PhoneBook::getfull(void)
+{
+	return (this->_full);
+}
+
+void PhoneBook::setfull(void)
+{
+	if (this->getfull() == 8)
+		this->_full = 7;
+}
+
+void PhoneBook::add_repertory(std::string firstName, std::string lastName,
+	std::string nickname, std::string phoneNumber, std::string secret)
+{
+	this->setsize();
+	this->setfull();
+	this->_contacts[_size].savecontact(firstName, lastName, nickname,
+		phoneNumber, secret, getsize());
+	_size++;
+	_full++;
+}
+
+bool PhoneBook::add_repertory()
+{
+	std::string firstName, lastName, nickname, phoneNumber, secret;
+	std::cout << "First name     : ";
+	std::getline(std::cin, firstName);
+	std::cout << "Last name      : ";
+	std::getline(std::cin, lastName);
+	std::cout << "Nickname       : ";
+	std::getline(std::cin, nickname);
+	std::cout << "Phone number   : ";
+	std::getline(std::cin, phoneNumber);
+	std::cout << "Darkest secret : ";
+	std::getline(std::cin, secret);
+	if (firstName.empty() || lastName.empty() || nickname.empty()
+		|| phoneNumber.empty() || secret.empty())
+	{
+		std::cout << "\033[31mPlease do not leave fields empty.\033[0m" << std::endl;
+		return (false);
+	}
+	this->add_repertory(firstName, lastName, nickname, phoneNumber, secret);
+	return (true);
 }
 
 void PhoneBook::show_directory() const
 {
-  for (int i = 0; i < _full; i++)
-  {
-    _contacts[i].showlittle();
-  }
+	for (int i = 0; i < _full; i++)
+	{
+		_contacts[i].showlittle();
+	}
 }
 
-void PhoneBook::show_contact(int index) const{
-  _contacts[index].showall();
+void PhoneBook::show_contact(int index) const
+{
+	_contacts[index].showall();
 }
