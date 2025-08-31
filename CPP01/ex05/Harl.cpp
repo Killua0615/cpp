a77759f6942f73fe9fc11f5a5cb268e8cf893c05
Killua <106412328+Killua0615@users.noreply.github.com>
2025-09-01 04:38:58 +0900

    ADD:ex05

---
+++ CPP01/ex05/Harl.cpp
@@ -0,0 +1,35 @@
+//Harl.cpp
+#include "Harl.hpp"
+
+void Harl::debug() {
+  std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+void Harl::info() {
+  std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+void Harl::warning() {
+  std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+}
+void Harl::error() {
+  std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(const std::string& level) {
+  typedef void (Harl::*Fn)();
+  struct Pair { const char* name; Fn fn; };
+  static const Pair table[] = {
+    { "DEBUG",   &Harl::debug },
+    { "INFO",    &Harl::info },
+    { "WARNING", &Harl::warning },
+    { "ERROR",   &Harl::error }
+  };
+  size_t i = 0;
+  while (i < 4) {
+    if (level == table[i].name) {
+      Fn f = table[i].fn;
+      (this->*f)();
+      return;
+    }
+    ++i;
+  }
+}
