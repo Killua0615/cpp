27898588b82337e3ab40cf8952f9fde7fa1f0220
Natsumi Teshima <nateshim@c4r3s15.42tokyo.jp>
2025-09-02 22:08:17 +0900

    ADD:hader

--- CPP01/ex06/Harl.hpp
+++ CPP01/ex06/Harl.hpp
@@ -1,16 +1,28 @@
-//Harl.hpp
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nateshim <nateshim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/02 22:07:08 by nateshim          #+#    #+#             */
+/*   Updated: 2025/09/02 22:07:10 by nateshim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HARL_HPP
-#define HARL_HPP
+# define HARL_HPP
 
-#include <string>
-#include <iostream>
+# include <iostream>
+# include <string>
 
-class Harl {
-public:
-  void debug();
-  void info();
-  void warning();
-  void error();
+class Harl
+{
+  public:
+	void debug();
+	void info();
+	void warning();
+	void error();
 };
 
 #endif
--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -1,34 +1,63 @@
-//main.cpp
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nateshim <nateshim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/02 22:07:13 by nateshim          #+#    #+#             */
+/*   Updated: 2025/09/02 22:07:15 by nateshim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Harl.hpp"
 
-static int levelIndex(const std::string& s) {
-  static const char* names[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-  int i = 0;
-  while (i < 4) {
-    if (s == names[i]) return i;
-    ++i;
-  }
-  return -1;
+static int	levelIndex(const std::string &s)
+{
+	static const char	*names[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int					i;
+
+	i = 0;
+	while (i < 4)
+	{
+		if (s == names[i])
+			return (i);
+		++i;
+	}
+	return (-1);
 }
 
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-    return 0;
-  }
-  Harl h;
-  int idx = levelIndex(argv[1]);
-  switch (idx) {
-    case 0:
-      std::cout << "[ DEBUG ]" << std::endl;   h.debug();   std::cout << std::endl;
-    case 1:
-      std::cout << "[ INFO ]" << std::endl;    h.info();    std::cout << std::endl;
-    case 2:
-      std::cout << "[ WARNING ]" << std::endl; h.warning(); std::cout << std::endl;
-    case 3:
-      std::cout << "[ ERROR ]" << std::endl;   h.error();   break;
-    default:
-      std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-  }
-  return 0;
+int	main(int argc, char **argv)
+{
+	Harl	h;
+	int		idx;
+
+	if (argc != 2)
+	{
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		return (0);
+	}
+	idx = levelIndex(argv[1]);
+	switch (idx)
+	{
+	case 0:
+		std::cout << "[ DEBUG ]" << std::endl;
+		h.debug();
+		std::cout << std::endl;
+	case 1:
+		std::cout << "[ INFO ]" << std::endl;
+		h.info();
+		std::cout << std::endl;
+	case 2:
+		std::cout << "[ WARNING ]" << std::endl;
+		h.warning();
+		std::cout << std::endl;
+	case 3:
+		std::cout << "[ ERROR ]" << std::endl;
+		h.error();
+		break ;
+	default:
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+	return (0);
 }
