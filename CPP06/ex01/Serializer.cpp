2491da3bfc0c4c03699c3d79dcaf77fc0ec3fcd7
Killua <106412328+Killua0615@users.noreply.github.com>
2026-02-16 20:59:23 +0900

    [ADD]  comment

--- CPP06/ex01/Serializer.cpp
+++ CPP06/ex01/Serializer.cpp
@@ -11,6 +11,7 @@ uintptr_t Serializer::serialize(Data* ptr)
 	return reinterpret_cast<uintptr_t>(ptr);
 }
 
+//↑serialised into an integer back into a Data* (pointer)
 Data* Serializer::deserialize(uintptr_t raw)
 {
 	return reinterpret_cast<Data*>(raw);
--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -23,3 +23,16 @@ int main()
 	std::cout << "restored->x = " << restored->x << std::endl;
 	return 0;
 }
+
+/*
+original is shown in hexadecimal bec it’s a pointer.
+raw is shown in decimal bec it’s an integer type.
+
+./serialize 
+original : 0x16d792ae8
+raw      : 6131624680
+restored : 0x16d792ae8
+OK
+restored->n = 42
+restored->x = 3.14
+*/
