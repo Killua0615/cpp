36c7e81735f97435cd79f25c51313f8eb8be99eb
Killua <106412328+Killua0615@users.noreply.github.com>
2026-03-02 16:17:18 +0900

    [ADD] coment

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -7,31 +7,31 @@
 template <typename T>
 class Array
 {
-private:
-	T* _data;
-	std::size_t _size;
+	private:
+		T* _data;
+		std::size_t _size;
 
-public:
-	class OutOfBoundsException : public std::exception
-	{
 	public:
-		virtual const char* what() const throw()
+		class OutOfBoundsException : public std::exception
 		{
-			return "Array index out of bounds";
-		}
-	};
+			public:
+				virtual const char* what() const throw()
+				{
+					return "Array index out of bounds";
+				}
+		};
 
-	Array();
-	Array(unsigned int n);
-	Array(const Array& other);
-	~Array();
+		Array();
+		Array(unsigned int n);
+		Array(const Array& other);
+		~Array();
 
-	Array& operator=(const Array& other);
+		Array& operator=(const Array& other);
 
-	T& operator[](std::size_t index);
-	const T& operator[](std::size_t index) const;
+		T& operator[](std::size_t index);
+		const T& operator[](std::size_t index) const;
 
-	std::size_t size() const;
+		std::size_t size() const;
 };
 
 //Code where the type can be determined later
